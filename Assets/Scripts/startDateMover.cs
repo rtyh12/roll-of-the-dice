77572d3dd64199f4dd39653c8c6b446c64a305c3
Mihai Verzan <rtyh12@gmail.com>
2022-07-17 15:35:36 +0300

    Put Start Date text movement in FixedUpdate

--- Assets/Scripts/startDateMover.cs
+++ Assets/Scripts/startDateMover.cs
@@ -2,19 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class startDateMover : MonoBehaviour
+public class StartDateMover : MonoBehaviour
 {
     public float speed = 4f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = new Vector3(transform.position.x+speed, transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
     }
 }
